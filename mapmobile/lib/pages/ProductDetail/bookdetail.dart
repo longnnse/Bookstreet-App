9cd306193d94ef7ad65690ab7bbc8ad2d856611d
longnnse <longnnse151484@fpt.edu.vn>
2025-03-08 15:03:13 +0700

    Enhance BookDetail page with additional book information and UI refinements
    - Added ISBN field to book details grid
    - Simplified GridView layout with shrinkWrap property
    - Added const constructors and minor UI optimizations

--- mapmobile/lib/pages/ProductDetail/bookdetail.dart
+++ mapmobile/lib/pages/ProductDetail/bookdetail.dart
@@ -42,7 +42,7 @@ class _BookDetailState extends State<BookDetail> {
             children: [
               Container(
                 margin: const EdgeInsets.only(bottom: 40),
-                child: Header(),
+                child: const Header(),
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -50,7 +50,7 @@ class _BookDetailState extends State<BookDetail> {
                   Flexible(
                       flex: 2,
                       child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
                         child: Container(
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(20),
@@ -62,7 +62,7 @@ class _BookDetailState extends State<BookDetail> {
                             crossAxisAlignment: CrossAxisAlignment.center,
                             children: [
                               Container(
-                                margin: EdgeInsets.only(bottom: 20),
+                                margin: const EdgeInsets.only(bottom: 20),
                                 child: NetworkImageWithFallback(
                                     imageUrl: product["urlImage"] ?? "",
                                     fallbackWidget: const Icon(Icons.error)),
@@ -73,7 +73,8 @@ class _BookDetailState extends State<BookDetail> {
                                     : "Chưa mở bán",
                                 onTap: () {
                                   if (product["storeId"] != null) {
-                                    context.push("/map/withStore/${product["storeId"]}");
+                                    context.push(
+                                        "/map/withStore/${product["storeId"]}");
                                   }
                                 },
                               )
@@ -82,141 +83,133 @@ class _BookDetailState extends State<BookDetail> {
                         ),
                       )),
                   Flexible(
-                      flex: 3,
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        child: Column(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(bottom: 20),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: Colors.white,
-                              ),
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 40, vertical: 20),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  DynamicText(
-                                    text: product['productName'] ?? "",
-                                    textStyle: const TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  DynamicText(
-                                    text: formatToVND(product['price'] ?? 0),
-                                    textStyle: const TextStyle(
-                                        color: Color.fromARGB(255, 186, 12, 0),
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  SizedBox(
-                                    height: 450,
-                                    child: GridView.count(
-                                      // shrinkWrap: true,
-                                      physics:
-                                          const NeverScrollableScrollPhysics(),
-                                      crossAxisCount: 2,
-                                      crossAxisSpacing: 1,
-                                      mainAxisSpacing: 1,
-                                      childAspectRatio: 5,
-                                      children: [
-                                        const DynamicText(text: ("Tác giả")),
-                                        DynamicText(
-                                            text: product?['book']
-                                                        ?['authors'] !=
-                                                    null
+                    flex: 3,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 20),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                DynamicText(
+                                  text: product['productName'] ?? "",
+                                  textStyle: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                DynamicText(
+                                  text: formatToVND(product['price'] ?? 0),
+                                  textStyle: const TextStyle(
+                                      color: Color.fromARGB(255, 186, 12, 0),
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                GridView.count(
+                                  shrinkWrap: true,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  crossAxisCount: 2,
+                                  crossAxisSpacing: 1,
+                                  mainAxisSpacing: 1,
+                                  childAspectRatio: 5,
+                                  children: [
+                                    const DynamicText(text: ("Tác giả")),
+                                    DynamicText(
+                                        text:
+                                            product?['book']?['authors'] != null
                                                 ? product?['book']?['authors']
                                                     .join(", ")
                                                 : ""),
-                                        const DynamicText(
-                                            text: ("Nhà xuất bản")),
-                                        DynamicText(
-                                            text: product['book']
-                                                    ?['publisherName'] ??
-                                                ""),
-                                        const DynamicText(
-                                            text: ("Nhà phân phối")),
-                                        DynamicText(
-                                            text: product['book']
-                                                    ?['distributorName'] ??
-                                                ""),
-                                        const DynamicText(
-                                            text: ("Ngày xuất bản")),
-                                        DynamicText(
-                                            text: product['book']
-                                                    ?['publicDay'] ??
-                                                "chưa có"),
-                                        const DynamicText(
-                                            text: ("Được bán tại")),
-                                        DynamicText(
-                                            text: product['storeName'] ??
-                                                "chưa có"),
-                                        const DynamicText(text: ("Tình trạng")),
-                                        DynamicText(
-                                          text: product['status'] == 1
-                                              ? "còn hàng"
-                                              : product['status'] == 2
-                                                  ? "hết hàng"
-                                                  : "không rõ",
-                                          textStyle: TextStyle(
-                                              color: product['status'] == 1
-                                                  ? Colors.green
-                                                  : Colors.red),
-                                        ),
-                                        const DynamicText(
-                                            text: ("Lần tái bản")),
-                                        DynamicText(
-                                            text: product['book']
-                                                        ?['editionNumber'] !=
-                                                    null
-                                                ? product['book']
-                                                        ['editionNumber']
-                                                    .toString()
-                                                : "1"),
-                                        const DynamicText(
-                                            text: ("Năm tái bản")),
-                                        DynamicText(
-                                            text: product['book']
-                                                        ?['editionYear'] !=
-                                                    null
-                                                ? product['book']['editionYear']
-                                                    .toString()
-                                                : "")
-                                      ],
+                                    const DynamicText(text: ("Nhà xuất bản")),
+                                    DynamicText(
+                                        text: product['book']
+                                                ?['publisherName'] ??
+                                            ""),
+                                    const DynamicText(text: ("Nhà phân phối")),
+                                    DynamicText(
+                                        text: product['book']
+                                                ?['distributorName'] ??
+                                            ""),
+                                    const DynamicText(text: ("Ngày xuất bản")),
+                                    DynamicText(
+                                        text: product['book']?['publicDay'] ??
+                                            "chưa có"),
+                                    const DynamicText(text: ("Được bán tại")),
+                                    DynamicText(
+                                        text:
+                                            product['storeName'] ?? "chưa có"),
+                                    const DynamicText(text: ("Tình trạng")),
+                                    DynamicText(
+                                      text: product['status'] == 1
+                                          ? "còn hàng"
+                                          : product['status'] == 2
+                                              ? "hết hàng"
+                                              : "không rõ",
+                                      textStyle: TextStyle(
+                                          color: product['status'] == 1
+                                              ? Colors.green
+                                              : Colors.red),
                                     ),
-                                  ),
-                                ],
-                              ),
+                                    const DynamicText(text: ("Lần tái bản")),
+                                    DynamicText(
+                                        text: product['book']
+                                                    ?['editionNumber'] !=
+                                                null
+                                            ? product['book']['editionNumber']
+                                                .toString()
+                                            : "1"),
+                                    const DynamicText(text: ("Năm tái bản")),
+                                    DynamicText(
+                                        text: product['book']?['editionYear'] !=
+                                                null
+                                            ? product['book']['editionYear']
+                                                .toString()
+                                            : ""),
+                                    const DynamicText(text: ("Isbn")),
+                                    DynamicText(
+                                        text: product['book']?['isbn'] != null
+                                            ? product['book']['isbn'].toString()
+                                            : "")
+                                  ],
+                                ),
+                              ],
                             ),
-                            Container(
-                              margin: EdgeInsets.only(bottom: 20),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: Colors.white,
-                              ),
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 40, vertical: 20),
-                              child: Column(
-                                children: [
-                                  const DynamicText(
-                                    text: "Mô tả sản phẩm",
-                                    textStyle: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  DynamicText(
-                                    text: product['description'] ?? "",
-                                    textStyle: const TextStyle(
-                                        fontWeight: FontWeight.w400),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ))
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 20),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 20),
+                            child: Column(
+                              children: [
+                                const DynamicText(
+                                  text: "Mô tả sản phẩm",
+                                  textStyle: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                DynamicText(
+                                  text: product['description'] ?? "",
+                                  textStyle: const TextStyle(
+                                      fontWeight: FontWeight.w400),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               )
             ],
